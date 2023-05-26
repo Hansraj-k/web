224f0735c9c09c159b052df9ab3b9524a79d8fa2
Hansraj <82136657+Hansraj-k@users.noreply.github.com>
2023-05-26 16:54:13 +0530

    Update share.js

--- share.js
+++ share.js
@@ -7,7 +7,7 @@ shareBtn.addEventListener('click', event => {
     // Browser supports native share api
     navigator.share({
       img: 'https://hansrajweb.netlify.app/assets/img/bg.jpg',
-      text: 'Digitizing Creativity Is All That We Do So Well ,   Check Our Creativity 👇🏻 & Digitalize Your Creativity: https://hansrajweb.netlify.app     Thanks and Regards ,    Websites By Hansraj Kondeda',
+      text: 'Digitizing Creativity Is All That We Do So Well ,   Check Our Creativity 👇🏻 & Digitalize Your Creativity: https://hansrajweb.netlify.app                 Thanks and Regards ,    Websites By Hansraj Kondeda',
       url: 'https://hansrajweb.netlify.app/',
       h1: 'Thanks & Regards, <br> Websites By Hansraj Kondeda'
     }).then(() => {
